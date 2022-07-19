b232284ae52ae5f8b982e994726cf5489c0b320a
Kateria Wynn <iamkateria@gmail.com>
2022-07-19 16:02:40 -0700

    added bg color to divs w/ click

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const gameContainer = document.getElementById("game");
+const gameContainer = document.getElementById('game');
 
 const COLORS = [
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple",
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple"
+  'red',
+  'blue',
+  'green',
+  'orange',
+  'purple',
+  'red',
+  'blue',
+  'green',
+  'orange',
+  'purple',
 ];
 
 // here is a helper function to shuffle an array
@@ -44,13 +44,13 @@ let shuffledColors = shuffle(COLORS);
 function createDivsForColors(colorArray) {
   for (let color of colorArray) {
     // create a new div
-    const newDiv = document.createElement("div");
+    const newDiv = document.createElement('div');
 
     // give it a class attribute for the value we are looping over
     newDiv.classList.add(color);
 
     // call a function handleCardClick when a div is clicked on
-    newDiv.addEventListener("click", handleCardClick);
+    newDiv.addEventListener('click', handleCardClick);
 
     // append the div to the element with an id of game
     gameContainer.append(newDiv);
@@ -58,9 +58,11 @@ function createDivsForColors(colorArray) {
 }
 
 // TODO: Implement this function!
-function handleCardClick(event) {
-  // you can use event.target to see which element was clicked
-  console.log("you just clicked", event.target);
+function handleCardClick(e) {
+  const color = e.target.className;
+  if (e.target.tagName === 'DIV') {
+    e.target.style.backgroundColor = `${color}`;
+  }
 }
 
 // when the DOM loads
